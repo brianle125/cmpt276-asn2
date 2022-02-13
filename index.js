e79bf6caaded46acf5f8de257c3edf945051a034
Brian <le.brian125@gmail.com>
2022-02-12 19:36:29 -0800

    trying to debug launch

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 app.get('/rectangles', (req,res)=>{
     // let data = { results: [1,2,3,4,5]};
-    res.render('pages/db');
     var getUsersQuery = 'SELECT * FROM rectangles';
     pool.query(getUsersQuery, (error,result) => {
         if(error)
