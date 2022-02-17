2d32ee45a93e215949a154326efa6107160ca299
Brian <le.brian125@gmail.com>
2022-02-17 14:41:09 -0800

    adjusting base table values; changing to id

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/rectangles/:name', async (req, res)=>{
 app.get('/edit/:name', async (req, res)=>{
   try {
     const client = await pool.connect();
-    const result = await client.query(`SELECT * FROM rects WHERE name = '${req.params.name}'`);
+    const result = await client.query(`SELECT * FROM rects WHERE id = '${req.params.name}'`);
     const results = { 'results': (result) ? result.rows : null};
     res.render('pages/edit', results);
     client.release();
